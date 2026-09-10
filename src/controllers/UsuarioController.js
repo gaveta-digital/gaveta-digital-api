d351434f1617799a29eaf0260204d262f05f0380
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-09-10 19:52:45 -0300

    feat: adiciona controller de login de usuário

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -6,12 +6,36 @@ const UsuarioController = {
             //1. Desestruturar os dados do corpo da requisição
             const { nome, email, senha } = req.body;
             //2. Chamar o serviço para criar o usuário
-            const usuarioCriado = await UsuarioService(nome, email, senha);
+            const usuarioCriado = await UsuarioService.criarUsuario(nome, email, senha);
             //3. Retornar a resposta com o usuário criado
             res.status(201).json({
                 status: 'sucesso',
                 data: usuarioCriado,
             });
+        } catch(erro){
+            //enviar o erro para o middleware de tratamento de erros
+            next(erro);
+        }
+    },
+
+    async loginUsuario(req, res, next){
+        try{
+            //1. desestruturar os dados do corpo da requisição
+            const { email, senha } = req.body;
+
+            //2. chamar o service para fazer o login
+            //como a função retorna um objeto com usuario e token, vamos guardar o resultado em uma variável
+            const {usuario, token} = await UsuarioService.loginUsuario(email, senha);
+            
+            //3. devolver a resposta com o token e os dados do usuário (sem a senha)
+            res.status(200).json({
+                status: 'sucesso',
+                data: {
+                    usuario,
+                    token,
+                },
+            });
+
         } catch(erro){
             //enviar o erro para o middleware de tratamento de erros
             next(erro);
