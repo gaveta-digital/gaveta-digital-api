8a3d8d23f66b721913b4cdaa93803cd101d5c53f
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-09-10 19:52:55 -0300

    feat: adiciona rota de login

--- src/routes/UsuarioRota.js
+++ src/routes/UsuarioRota.js
@@ -4,5 +4,6 @@ const UsuarioController= require ('../controllers/UsuarioController')
 const router = Router();
 
 router.post('/usuarios', UsuarioController.criarUsuario);
+router.post('/login', UsuarioController.loginUsuario);
 
 module.exports = router;
