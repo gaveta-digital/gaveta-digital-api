07c05c6b533032d32bfcc6ac531fc174cfcc3fb6
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-09-11 10:54:57 -0300

    feat: integra middlewares de validação nas rotas de usuário

--- src/routes/UsuarioRota.js
+++ src/routes/UsuarioRota.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
 const UsuarioController= require ('../controllers/UsuarioController')
+const validarUsuario = require('../middlewares/validarUsuario');
+const validarLogin = require('../middlewares/validarLogin');
 
 const router = Router();
 
-router.post('/usuarios', UsuarioController.criarUsuario);
-router.post('/login', UsuarioController.loginUsuario);
+router.post('/usuarios', validarUsuario, UsuarioController.criarUsuario);
+router.post('/login', validarLogin, UsuarioController.loginUsuario);
 
 module.exports = router;
